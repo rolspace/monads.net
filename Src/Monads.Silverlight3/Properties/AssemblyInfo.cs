ec06a0dbca88519c2536777b767268ce9db1feaf
rolspace <rolandorramos@gmail.com>
2017-02-19 00:20:15 +0100

    Updated .sln file to for VS2015 support and removed Silverlight 3 and 4 projects

--- Src/Monads.Silverlight3/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("87d114e5-19ec-4806-aae8-070d7b64e8fb")]
--- Src/Monads.Silverlight4/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("3aa2c5e5-d54e-4137-85a5-8f818fb33ca2")]
