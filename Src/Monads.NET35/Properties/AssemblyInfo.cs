6bd9c11bec2499e1b7e67a3388d866851f09602a
rolspace <rolandorramos@gmail.com>
2017-02-19 18:41:44 +0100

    Removed NET35 and NET4 projects

--- Src/Monads.NET35/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("5dfa6a4a-d879-4aab-8e13-e58ccdff06ad")]
--- Src/Monads.NET4/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("d8793fc7-1963-4984-bfe7-57466c84a630")]
