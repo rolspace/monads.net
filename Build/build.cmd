574cea5b730209414890fb2673c393077e87ba8e
rolspace <rolandorramos@gmail.com>
2017-04-14 22:29:00 +0200

    Renamed _release.msbuild to release.msbuild

--- Build/build.cmd
+++ Build/build.cmd
@@ -1,3 +1,3 @@
 REM due to changes in the location of MSBuild.exe in VS2017 we will add the MSBuild location to the PATH env. variable
 @cls
-@MSBuild Scripts\_release.msbuild /fileLogger %*
+@MSBuild Scripts\release.msbuild /fileLogger %*
